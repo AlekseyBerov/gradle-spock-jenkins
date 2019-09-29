d6a684a3965e5a4994921011d348ef2e505bac5a
Aleksey Berov <krusual@live.com>
2019-09-29 17:26:24 +0300

    style

--- src/com/berov/algos/sort/Bubble.groovy
+++ src/com/berov/algos/sort/Bubble.groovy
@@ -1,11 +1,11 @@
-package com.berov.algos.sort
+package com.berov.algos.sort;
 
 class Bubble {
 
-  private final def mock
+  private final def mock;
 
   Bubble(def mock) {
-    this.mock = mock
+    this.mock = mock;
   }
 
   def sort(arr) {
@@ -15,16 +15,15 @@ class Bubble {
     }
 
     int temp = 0;
-      for(int i=0; i < n; i++){
-          for(int j=1; j < (n-i); j++){
-            if(arr[j-1] > arr[j]){
-              temp = arr[j-1];
-              arr[j-1] = arr[j];
-              arr[j] = temp;
-            }
+      for(int i=0; i < n; i++) {
+        for(int j=1; j < (n-i); j++) {
+          if(arr[j-1] > arr[j]){
+            temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
           }
+        }
       }
     return arr;
   }
-
 }
--- src/com/berov/algos/sort/Select.groovy
+++ src/com/berov/algos/sort/Select.groovy
@@ -1,11 +1,11 @@
-package com.berov.algos.sort
+package com.berov.algos.sort;
 
 class Select {
 
-  private final def mock
+  private final def mock;
 
   Select(def mock) {
-    this.mock = mock
+    this.mock = mock;
   }
 
   def sort(arr) {
@@ -14,7 +14,7 @@ class Select {
       return arr;
     }
 
-    for (int i = 0; i < n-1; i++) {
+    for (int i = 0; i < (n-1); i++) {
       def min = i;
       for (int y = i + 1; y < n; y++)
         if (arr[y] < arr[min]) {
@@ -24,13 +24,7 @@ class Select {
         arr[min] = arr[i];
         arr[i] = tmp;
     }
-    println "In the end we have $arr"
+    println "In the end we have $arr";
     return arr;
-
-
   }
-
-
-
-
 }
