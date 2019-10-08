00b23fae637edc4330188685cdf01fe2da95acd7
AlekseyBerov <krusual0@outlook.com>
2019-10-09 02:18:33 +0300

    array fib solution

--- src/com/berov/algos/basic/Fibonacci.groovy
+++ src/com/berov/algos/basic/Fibonacci.groovy
@@ -3,9 +3,14 @@ package com.berov.algos.basic;
 class Fibonacci {
 
   def fib(int n) {
-
-
-    return n;
+    if (n <= 1) {
+      return n;
+    }
+    fib = [0,1] + [0] * (n - 1);
+    (2..n).each {
+        fib[it] = fib[it - 1] + fib[it - 2];
+    }
+    return fib[-1];
   }
 
 }
